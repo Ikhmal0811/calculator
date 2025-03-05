ed283c5de55488ca960c3f6ee6be93dd10e05b21
malmi <mhd1ikhmal@gmail.com>
2025-03-05 22:58:17 +0800

    finally finish the back end

--- calculator.js
+++ calculator.js
@@ -4,31 +4,38 @@ let buttonNumber = document.querySelectorAll("#number")
 let buttonSymbol = document.querySelectorAll("#symbol")
 let buttonResult = document.getElementById("equalValue")
 let buttonClear = document.getElementById("clear")
+let answer = document.getElementById("answer")
 let result = document.getElementById("result")
-let output = "";  //maybe useful for something
-let temp = [];
+let output = 0;  //maybe useful for something
+let temp = [2,3];
+let ans = 0;
 
 
 
 
 //all the function for +,-,x & /
-function addition(...inputNumber){
+function addition(inputNumber){
+    
+    
     let add = 0;    
     for(i=0;i<inputNumber.length;i++){
         add += inputNumber[i]
-    }
-    return add;
+    }        
+    return add;    
 }
 
-function subtraction(...inputNumber){
-    let minus = 0;    
-    for(i=0;i<inputNumber.length;i++){
+
+function subtraction(inputNumber){
+    let minus = inputNumber[0];    
+    for(i=1;i<inputNumber.length;i++){
         minus -= inputNumber[i]
     }
     return minus;
 }
 
-function multiplication(...inputNumber){
+function multiplication(inputNumber){
+    console.log(inputNumber);
+    
     let multiply = 1;    
     for(i=0;i<inputNumber.length;i++){
         multiply *= inputNumber[i]
@@ -36,9 +43,12 @@ function multiplication(...inputNumber){
     return multiply;
 }
 
-function division(...inputNumber){
-    let devide = 1;    
-    for(i=0;i<inputNumber.length;i++){
+
+
+
+function division(inputNumber){
+    let devide = inputNumber[0];    
+    for(i=1;i<inputNumber.length;i++){
         devide /= inputNumber[i]
     }
     return devide;
@@ -53,11 +63,24 @@ buttonNumber.forEach(button =>{
     }); 
 })
 
+//when click the arithmetic symbol will choose which condition will happen when preess "="
 buttonSymbol.forEach(button =>{
     button.addEventListener("click",function(){
-        temp.push(result.innerText)
+        temp.push(Number(result.innerText))
+        console.log(temp);
+        
         result.innerText = "";
-                
+        if(button.innerText == "+"){
+            output = 1
+        }else if(button.innerText == "-"){
+            output = 2
+        }else if(button.innerText == "x"){
+            output = 3
+        }else{
+            output = 4
+        }
+        
+               
     })
 })
 
@@ -70,18 +93,34 @@ buttonSymbol.forEach(button =>{
 
 buttonResult.addEventListener("click",resultCal)
 
-
+//function when push the button '=' will choose between the calculation fx
 function resultCal(){
-    temp.push(result.innerText)
-     console.log(temp);
+    temp.push(Number(result.innerText))
+    
+     if(output === 1){
+        ans = addition(temp)                        
+     }else if(output === 2){
+        ans = subtraction(temp)       
+     }else if(output === 3){
+        ans = multiplication(temp)
+     }else{
+        ans = division(temp)
+     }
+     console.log(ans);
+     result.innerText = "";
+     answer.innerText = ans
      
           
 }
 
+
+// function to clear the screen
 buttonClear.addEventListener("click",function(){
     result.innerText = "";
     output = "";
     temp = [];
+    ans = 0;
+    answer.innerText = "";
 })
 
 
